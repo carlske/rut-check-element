6b23a490ee21c8cf286bb65433797d60e908ca64
pythonicv <carlosvalentin.diaz@decathlon.com>
2024-10-17 12:57:48 -0600

    feat: added initial files project

---
+++ test/karma.config.js
@@ -0,0 +1,20 @@
+module.exports = function (config) {
+    config.set({
+        frameworks: ['mocha', 'chai'],
+        files: [{pattern: '../dist/index.js', type: 'module'}, 'test.js'],
+        reporters: ['mocha'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        browsers: ['ChromeHeadlessNoSandbox'],
+        customLaunchers: {
+            ChromeHeadlessNoSandbox: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox']
+            }
+        },
+        autoWatch: false,
+        singleRun: true,
+        concurrency: Infinity
+    })
+}
