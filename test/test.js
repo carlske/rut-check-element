520fd81e1213b2e4e3369e24ef0baf8074d2f3f6
pythonicv <carlosvalentin.diaz@decathlon.com>
2024-10-21 16:19:32 -0600

    feat: changed chia to jest for testing

--- test/test.js
+++
@@ -1,7 +0,0 @@
-describe('text-expander limits the lookBack after commit', function () {
-    it('does not match if lookBackIndex is bigger than activation key index', function () {
-        const found = true
-        assert(found === true)
-    })
-
-})
