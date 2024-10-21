84fcd4dab41b3ac66349011cf157c8bec2822c6f
pythonicv <carlosvalentin.diaz@decathlon.com>
2024-10-21 13:45:44 -0600

    test: renamed karma config file

---
+++ test/test.js
@@ -0,0 +1,7 @@
+describe('text-expander limits the lookBack after commit', function () {
+    it('does not match if lookBackIndex is bigger than activation key index', function () {
+        const found = true
+        assert(found === true)
+    })
+
+})
