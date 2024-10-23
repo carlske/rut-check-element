041ec1df99d32020f13db6148fc5d91117d3b853
pythonicv <carlosvalentin.diaz@decathlon.com>
2024-10-23 11:27:40 -0600

    chore: copy index.html to dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,25 @@
 //const resolve = require('rollup-plugin-node-resolve')
 import typescript from '@rollup/plugin-typescript';
+import copy from 'rollup-plugin-copy'
 
 
 export default [
     {
         input: 'src/index.ts',
-        output: {
-            file: "dist/index.js",
-            format: "cjs",
-            sourcemap: false,
-        },
-        plugins: [typescript()]
+        output: [
+            {
+                file: "dist/index.js",
+                format: "cjs",
+                sourcemap: false,
+            }
+        ],
+        plugins: [
+            typescript(),
+            copy({
+                targets: [
+                    { src: 'examples/index.html', dest: 'dist' },
+                ]
+            })
+        ]
     }
 ]
